e02f18f1d13e05815be9dc112d39a1be055612ff
Julia <juliazhang1108@gmail.com>
2020-01-01 16:16:09 -0500

    VUE TS LATEX

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <vue-mathjax :formula="formula"></vue-mathjax>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { VueMathjax } from 'vue-mathjax';
+
+@Component({
+  components: {
+    'vue-mathjax': VueMathjax
+  }
+})
+export default class App extends Vue {
+  private formula: string = "$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$";
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
